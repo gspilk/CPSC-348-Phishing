ef4ca5781724f3af9f9c1b5a8d622d5cf6a21567
Charlie <45837976+cbennington852@users.noreply.github.com>
2025-04-19 13:21:55 -0700

    add logging

--- server.js
+++ server.js
@@ -6,19 +6,26 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const publicDir = path.join(__dirname, 'public');
 
+ const output = fs.createWriteStream('./logs.txt', { flags: 'w' });
+ const errorOutput = fs.createWriteStream('./error.txt', { flags: 'w' });
+ const logger = new console.Console(output, errorOutput);
+
+    logger.log('This will be written to output.log');
+    logger.error('This will be written to error.log');
+
+
 const server = http.createServer((req, res) => {
-  
   if (req.method === 'POST') {
     let body = [];
     req.on('data', chunk => {
       body.push(chunk);
     }).on('end', () => {
       body = Buffer.concat(body).toString();
-      console.log('Received POST request:');
-      console.log('  Method:', req.method);
-      console.log('  URL:', req.url);
-      console.log('  Headers:', req.headers);
-      console.log('  Body:', body);
+      logger.log('Received POST request:');
+      logger.log('  Method:', req.method);
+      logger.log('  URL:', req.url);
+      logger.log('  Headers:', req.headers);
+      logger.log('  Body:', body);
       res.statusCode = 200;
       res.end('POST request received and logged.');
     });
@@ -32,14 +39,13 @@ server.on
 
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    logger.log(`Server running at http://${hostname}:${port}/`);
 });
 
 function httpHandler(req, res) {
      fs.readFile('./public/' + req.url, function (err, data) {
 
          if (err == null ) {
-
              res.writeHead(200, {'Content-Type': 'text/html'});
              res.write(data);
              res.end();
